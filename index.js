dc52ce4c91fb744cad771258acf14b66fbba2fc7
Luciano Ordoñez <lucianordonez123@gmail.com>
2023-06-20 00:44:28 -0300

    inicializacion

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const {mongoose} = require('./database');
+var app = express();
+//middlewares
+app.use(express.json());
+app.use(cors({origin: 'http://localhost:4200'}));
+//setting
+app.set('port', process.env.PORT || 3000);
+//starting the server
+app.listen(app.get('port'), () => {
+console.log(`Server started on port`, app.get('port'));
+});
