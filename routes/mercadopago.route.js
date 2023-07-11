6b4dc742ed2e1b637eef8c50c32e605c2c6df927
Luciano Ordoñez <lucianordonez123@gmail.com>
2023-07-11 02:48:44 -0300

    configuracion del mp.route

--- routes/mercadopago.route.js
+++ routes/mercadopago.route.js
@@ -5,40 +5,47 @@ const mercadopago = require("mercadopago");
 const qrCode = require('qrcode');
 
 mercadopago.configure({
-    //access token de prueba de mi cuenta ajsj deberian usar de su cuenta pero fijense si funca 
-    access_token: "APP_USR-1546690050022270-012519-8faec084bd0e1627edb05a0877a7aadd-1295500452"
+  //access token de prueba de mi cuenta ajsj deberian usar de su cuenta pero fijense si funca 
+  access_token: "APP_USR-6274633563174333-071021-e4419b3e7b9687f7d75f6d6200ddaf1c-1420666480"
 })
-router.get("/crearpago", (req, res) => {
-  const { precio, descripcion } = req.body;
+router.post("/crearpago", (req, res) => {
+  /* const { precio, descripcion } = req.body; */
+  console.log(req.body.tipo)
+  let detalle = []; //Puede ser una lista de insumos o una cuota
+  if (req.body.tipo == "insumos") {
+    console.log(req.body.insumos)
+    for (let i of req.body.insumos) {
+      detalle.push({ title: i.nombre, unit_price: i.precio, quantity: 1 });
+    }
+    console.log(detalle);
+  }
+  else if (req.body.tipo == "cuota") {
+    detalle.push({ title: "Pago cuota", unit_price: req.body.cuota.importe, quantity: 1 })
+  }
+
   let preference = {
     back_urls: {
       success: "http://localhost:3000/success",
     },
-    items: [
-      {
-        title: descripcion,
-        unit_price: precio,
-        quantity: 1,
-      },
-    ],
-    notification_url: "http://localhost:3000/notificaciones",
+    items: detalle,
+    /* notification_url: "http://localhost:3000/notificaciones", */
   };
 
   mercadopago.preferences
     .create(preference)
     .then(function (response) {
-        const initPoint = response.body.init_point;
+      const initPoint = response.body.init_point;
 
-        // Genera el código QR a partir de la URL del init_point
-        qrCode.toDataURL(initPoint, function (err, qrDataURL) {
-          if (err) {
-            console.error('Error al generar el código QR:', err);
-            res.status(500).send('Error al generar el código QR');
-          } else {
-            // Envia el código QR como respuesta
-            res.status(200).json({message: qrDataURL, status: '1'});
-          }
-        });
+      // Genera el código QR a partir de la URL del init_point
+      qrCode.toDataURL(initPoint, function (err, qrDataURL) {
+        if (err) {
+          console.error('Error al generar el código QR:', err);
+          res.status(500).send('Error al generar el código QR');
+        } else {
+          // Envia el código QR como respuesta
+          res.status(200).json({ link: initPoint, qr: qrDataURL, status: '1' });
+        }
+      });
     })
     .catch(function (error) {
       console.log(error);
@@ -47,46 +54,46 @@ router.get("/crearpago", (req, res) => {
 
 //no funciona, intente con ngrok pero tampoco
 router.post("/notificaciones", async (req, res) => {
-    console.log("######################notificarr####################################");
-    const {query} = req
-    const topic = query.topic || query.type;
-    console.log("#############topic################", /* topic */);
-  
-    switch (topic) {  
-      case "payment":
-        const paymentId = query.id || query["data.id"];
-        /* console.log(topic, "getting payment", paymentId); */
-        
-        const payment = await mercadopago.payment.findById(paymentId);
-        /* console.log(payment);
-   */
-        var {body} = await mercadopago.merchant_orders.findById(
-          payment.body.order.id
-        );
-        break;
-      case "merchant_order":
-        const orderId = query.id;
-        console.log(topic, "getting payment", orderId);
-        var {body} = await mercadopago.merchant_orders.findById(orderId);
-        break;
+  console.log("######################notificarr####################################");
+  const { query } = req
+  const topic = query.topic || query.type;
+  console.log("#############topic################", /* topic */);
+
+  switch (topic) {
+    case "payment":
+      const paymentId = query.id || query["data.id"];
+      /* console.log(topic, "getting payment", paymentId); */
+
+      const payment = await mercadopago.payment.findById(paymentId);
+      /* console.log(payment);
+ */
+      var { body } = await mercadopago.merchant_orders.findById(
+        payment.body.order.id
+      );
+      break;
+    case "merchant_order":
+      const orderId = query.id;
+      console.log(topic, "getting payment", orderId);
+      var { body } = await mercadopago.merchant_orders.findById(orderId);
+      break;
+  }
+  //esto seria la informacion real y nesesaria para saber el estado de la orden
+  console.log('merchantOrder###################### ### ###', body.payments)
+  var paidAmount = 0
+
+  body.payments.forEach(function (payment) {
+    if (payment.status === 'approved') {
+      paidAmount += payment.transaction_amount;
     }
-    //esto seria la informacion real y nesesaria para saber el estado de la orden
-    console.log('merchantOrder###################### ### ###', body.payments)
-    var paidAmount = 0
-  
-    body.payments.forEach(function (payment) {
-        if (payment.status === 'approved') {
-          paidAmount += payment.transaction_amount;
-        }
-      });
-    if(paidAmount >= body.total_amount){
-      console.log('PAGADOOOOO')
-    }else{
-      console.log('NOOO PAGADOOOOO')
-    }
-  
-    res.send()
-    res.status(200)
+  });
+  if (paidAmount >= body.total_amount) {
+    console.log('PAGADOOOOO')
+  } else {
+    console.log('NOOO PAGADOOOOO')
+  }
+
+  res.send()
+  res.status(200)
 });
 
 module.exports = router;
